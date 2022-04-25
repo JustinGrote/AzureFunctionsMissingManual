b74b5bdc2f82bb9d2f2dd479ba9ed94f763fe1cc
Justin Grote <JustinGrote@users.noreply.github.com>
2022-04-25 07:34:55 -0700

    first commit

---
+++ AzureFunctionsMissingManual.psm1
@@ -0,0 +1,9 @@
+
+function CreateUser {
+    param($name)
+
+    $sleepInterval = (Get-Random -min 2000 -max 5000) / 1000
+    "Creating User $name!"
+    Start-Sleep $sleepInterval
+    "$name Completed! It took $sleepInterval seconds to create the user"
+}
